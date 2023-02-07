6922ac80a75cc137e237a4dd9547a426c907d234
Leo Banos Garcia <bsleo625@gmail.com>
2023-02-07 12:55:43 -0700

    Adds endpoint for get all items

--- spec/requests/api/items_request_spec.rb
+++ spec/requests/api/items_request_spec.rb
@@ -21,4 +21,21 @@ describe "Items API" do
       expect(item[:attributes]).to have_key(:merchant_id)
     end
   end
+
+  it "get one item by id" do
+    get "/api/v1/items/#{@item_1.id}"
+
+    expect(response).to be_successful
+
+    item = JSON.parse(response.body, symbolize_names: true)[:data]
+
+    expect(item[:attributes]).to have_key(:name)
+    expect(item[:attributes][:name]).to be_a(String)
+    expect(item[:id]).to eq(@item_1.id.to_s)
+    expect(item[:attributes]).to have_key(:description)
+    expect(item[:attributes][:description]).to be_a(String)
+    expect(item[:attributes]).to have_key(:unit_price)
+    expect(item[:attributes][:unit_price]).to be_a(Float)
+    expect(item[:attributes]).to have_key(:merchant_id)
+  end
 end
