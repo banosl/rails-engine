7aba0c6b01730c4d367d92c3ea2002336dbb96e9
Leo Banos Garcia <bsleo625@gmail.com>
2023-02-06 20:48:16 -0700

    adds merchants show

--- spec/requests/api/merchants_request_spec.rb
+++ spec/requests/api/merchants_request_spec.rb
@@ -4,11 +4,12 @@ describe "Merchants API" do
   before :each do
     load_test_data
   end
+
   it "sends a list of all merchants" do
     get '/api/v1/merchants'
 
     expect(response).to be_successful
-# binding.pry
+
     merchants = JSON.parse(response.body, symbolize_names: true)[:data]
 
     expect(merchants.count).to eq(3)
@@ -17,5 +18,17 @@ describe "Merchants API" do
       expect(merchant[:attributes]).to have_key(:name)
       expect(merchant[:attributes][:name]).to be_a(String)
     end
+  end
+
+  it "returns one merchant based on id" do
+    get "/api/v1/merchants/#{@merchant_1.id}"
+
+    expect(response).to be_successful
+
+    merchant = JSON.parse(response.body, symbolize_names: true)[:data]
+
+    expect(merchant[:attributes]).to have_key(:name)
+    expect(merchant[:attributes][:name]).to be_a(String)
+    expect(merchant[:id]).to eq(@merchant_1.id.to_s)
   end
 end
