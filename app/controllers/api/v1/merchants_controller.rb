274e21a292005011a7a5d9db7705048b7b8d5a5b
Leo Banos Garcia <bsleo625@gmail.com>
2023-02-06 20:36:59 -0700

    Adds Merchant serializer

--- app/controllers/api/v1/merchants_controller.rb
+++ app/controllers/api/v1/merchants_controller.rb
@@ -1,6 +1,6 @@
 class Api::V1::MerchantsController < ApplicationController
   def index
     # binding.pry
-    render json: Merchant.all.order(:id)
+    render json: MerchantSerializer.new(Merchant.all)
   end
 end
