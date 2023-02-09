1cb841eef514b8a3976f3804bde44896eea78eb7
Leo Banos Garcia <bsleo625@gmail.com>
2023-02-08 18:22:11 -0700

    removes pry

--- app/controllers/api/v1/merchants/search_controller.rb
+++ app/controllers/api/v1/merchants/search_controller.rb
@@ -3,7 +3,6 @@ class Api::V1::Merchants::SearchController < ApplicationController
     if params.has_key?(:name)
       merchants = Merchant.where("lower(name) like ?", "%#{params[:name].downcase}%").order(:name)
       render json: MerchantSerializer.new(merchants)
-      # binding.pry
     end
   end
 end
